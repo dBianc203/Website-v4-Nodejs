5a57b26f58325cc518e0ed96e1ddf770ac86af7d
Devon <62315100+dBianc203@users.noreply.github.com>
2026-08-22 21:53:34 -0400

    Add files via upload

---
+++ skills.ts
@@ -0,0 +1,45 @@
+export const skills = {
+  networking: [
+    "Cisco",
+    "Juniper",
+    "Aruba",
+    "Routing & Switching",
+    "VLAN",
+    "VRF",
+    "OSPF",
+    "BGP",
+    "802.1X",
+    "RADIUS",
+    "SNMP",
+  ],
+
+  automation: [
+    "Python",
+    "Groovy",
+    "REST APIs",
+    "Cisco Catalyst Center",
+  ],
+
+  monitoring: [
+    "LogicMonitor",
+    "Graylog",
+    "Splunk",
+    "ThousandEyes",
+    "Datadog",
+  ],
+
+  security: [
+    "Identity Service Engine (ISE)",
+    "StealthWatch",
+    "SGTs",
+    "Firewalls",
+    "VPNs",
+  ],
+
+  systems: [
+    "Linux",
+    "RHEL",
+    "Proxmox",
+    "VMware",
+  ],
+};
